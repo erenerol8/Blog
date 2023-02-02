b7865f5d1a2d0a86cc25a5d3441ac3d27a842880
Eren <erenerol8@gmail.com>
2023-02-03 01:21:46 +0300

    fixed bugs

---
+++ Blog.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Blog.Service.Services.Abstractions;
+using Blog.Service.Services.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Areas.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    { 
+        private readonly IArticleServices articleServices;
+
+        public HomeController(IArticleServices articleServices)
+        {
+        this.articleServices = articleServices;
+        }
+       
+        public async Task<IActionResult> Index()
+        {
+           var articles = await articleServices.GetAllArticlesAsync();
+            return View(articles);
+        }
+    }
+}
